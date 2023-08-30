4b3c26e834797a3a639c720972e725efefb6e609
TommyMynnSon <tommy.m.son@gmail.com>
2023-08-29 20:19:51 -0700

    Setup text content and column styling

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,5 +1,17 @@
+import QRCode from '../QRCode/QRCode';
+import './Card.css';
+
 const Card = () => {
-    return <div>Card</div>;
+    return (
+        <div className='card'>
+            <QRCode />
+            <p>Improve your front-end skills by building projets</p>
+            <p>
+                Scan the QR code to visit Frontend Mentor and take your coding
+                skills to the next level
+            </p>
+        </div>
+    );
 };
 
 export default Card;
