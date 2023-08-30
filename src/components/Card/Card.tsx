61344286451117af66c8dd062924283d3f396cdc
TommyMynnSon <tommy.m.son@gmail.com>
2023-08-29 20:46:38 -0700

    Add styling to Card component, background color for body

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -6,7 +6,7 @@ const Card = () => {
         <div className='card'>
             <QRCode pathToQRCodeImage='src/assets/image-qr-code.png' />
             <p>Improve your front-end skills by building projets</p>
-            <p>
+            <p className={'description'}>
                 Scan the QR code to visit Frontend Mentor and take your coding
                 skills to the next level
             </p>
